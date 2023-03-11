d09ac043c721243792af5b106e14e8792f66afd6
Peter Aryanto <peter.aryanto@gmail.com>
2023-03-11 21:43:54 +1100

    Created 1st component named StorePicker.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import StorePicker from './components/StorePicker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    <StorePicker />
   </React.StrictMode>
 );
 
